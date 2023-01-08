6c21b5356a74a48fd2a59f1f90c49dedb2de9a61
tilen <tilen@DESKTOP-QMC9V1L>
2023-01-08 07:14:53 +0100

    update

--- web/Entities/Comment.cs
+++
@@ -1,9 +0,0 @@
-namespace web.Entities 
-{
-    public class Comment 
-    {
-        public int Id { get; set; }
-        public string Body { get; set; }
-        public User CreatedBy { get; set; }
-    }
-}
