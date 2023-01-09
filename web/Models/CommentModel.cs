e61006ccf26fe1366b32e0ecf1d649f3009d249d
tilen <tilen@DESKTOP-QMC9V1L>
2023-01-09 07:42:37 +0100

    revert

--- web/Models/CommentModel.cs
+++
@@ -1,6 +0,0 @@
-namespace web.Models {
-    public class CommentModel {
-        public int Id { get; set; }
-        public string Body { get; set; }
-    }
-}
--- web/Models/UserModel.cs
+++
@@ -1,8 +0,0 @@
-namespace web.Models {
-    public class UserModel {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-    }
-}
